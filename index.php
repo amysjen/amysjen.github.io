824a85ac6188b637933def357334ef379b5920f3
Amy <amysuejen@gmail.com>
2024-08-07 13:56:26 -0700

    updating landing page

--- index.php
+++ index.php
@@ -1,101 +1,54 @@
 <!doctype html>
-<html>
+<html lang="en">
 <head>
 <meta charset="utf-8">
+<meta name="google" content="notranslate">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Tucson Web Developer and Designer | Amy Jen</title>
+<title>Portfolio of Amy Jen in Front-End Web Development</title>
 <meta name="description" content="Amy Jen is a web designer and developer in the region of Tucson, Arizona.">
 <link rel="shortcut icon" href="favicon.ico" >
-<link rel="stylesheet" type="text/css" href="/css/main.css">
+<link rel="stylesheet" type="text/css" href="/assets/css/main.css">
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-<script type="text/javascript" src="/js/main.js"></script>
+<script type="text/javascript" src="/assets/scripts/main.js"></script>
 </head>
 
 <body>
-<header>
-	<div class="content">
-		<img src="/images/logo-white.png" alt="Amy Jen" class="logo">
-		<span class="occupation">Web <span class="em">Designer</span> | <span class="em">Developer</span></span>
-	</div>
-	<a href="#skills" class="arrow">&rsaquo;</a>
-</header>
 
-<div id="skills">
-	<div class="content">
-		<div id="spotlight">
-			<div class="skill">
-				<img src="/images/ic-responsive.png" class="icon" alt="Responsive design icon">
-				<span>Responsive Design</span>
-			</div>
-			<div class="skill">
-				<img src="/images/ic-ux.png" class="icon" alt="user experience icon">
-				<span>User Experience</span>
-			</div>
-			<div class="skill">
-				<img src="/images/ic-ui.png" class="icon" alt="user interface icon">
-				<span>User Interface</span>
-			</div>
-		</div>
-		<span class="intro">Coding Proficiency</span>
-		<ul class="list">
-			<li>HTML5</li>
-			<li>CSS3</li>
-			<li>Javascript</li>
-			<li>PHP</li>
-		</ul>
+	<div id="topHeader">
+		<img src="/assets/images/logo.png" alt="Amy Jen logo" class="logo">
+		<div class="titles">
+			<span>Front-End Development</span> | 
+			<span>UX/UI</span></div>
 	</div>
-</div>
 
-<div id="about">
-	<img src="/images/amy-jen.jpg" alt="Amy Jen" class="profile">
-	<h1 class="intro">Hello! I'm Amy.</h1>
-	<span class="bio">I'm a web designer and front-end developer residing in Arizona, United States. I love and enjoy creating for the desktop and mobile web.</span>
-	<!-- <a href="#portfolio" class="arrow">&rsaquo;</a> -->
-</div>
 
-<div class="intro">Portfolio</div>
-<div id="portfolio" class="carousel slide">
-	<div class="carousel-inner">
-		<div class="item active">
-			<div class="info">
-				<span class="title">Tucson Natural Medicine Center</span>
-				<span class="detail">
-					The goal for this website is to educate the visitors on the different styles of acupuncture and other Eastern natural medicine. The main call-to-action is to have visitors call in for consultations and appointments. 
-				</span>
-				<a href="http://tucsonnaturalmedicine.com" target="_blank" class="link">View Website</a>
-			</div>
-			<img src="/images/img-tnmc.jpg" alt="Tucson Natural Medicine Center">
-		</div>
-		<div class="item">
-			<div class="info">
-				<span class="title">Mars Artwork</span>
-				<span class="detail">
-					This was built as a portfolio to display this artist's best work of illustrations.
-				</span>
-				<a href="http://marsartwork.com" target="_blank" class="link">View Website</a>
-			</div>
-			<img src="/images/img-marsartwork.jpg" alt="Mars Artwork">
-		</div>
-	</div>
-	<a class="carousel-control left" href="#portfolio" data-slide="prev">&lsaquo;</a>
-    <a class="carousel-control right" href="#portfolio" data-slide="next">&rsaquo;</a>
-    <ol class="carousel-indicators">
-    	<li data-target="#portfolio" data-slide-to="0" class="active"></li>
-	    <li data-target="#portfolio" data-slide-to="1" ></li>
-    </ol>
-</div>
+    <div id="videoClip">
+      <svg xmlns="http://www.w3.org/2000/svg" style="position:absolute">
+        <defs>
+            <clipPath id="mask">
+            <path d="M0,370.6c-0.5,35,29.5,68,60,82.8c40.8,19.7,82.2,6.8,99.3,1.4c76-23.8,83.3-81.4,130.3-79.9c43.9,1.4,53.2,52.1,102.2,52.5
+          c38.6,0.3,67.2-30.9,79.9-44.6c44.4-48.3,49.5-116.9,33.1-165.5c-6-17.7-11.5-34.1-27.4-45.3c-30.9-22-62.5,2.7-96.2-16.2
+          c-27.9-15.7-17.7-39-44.8-68c-44.9-47.9-125.5-40.2-155.5-37.3C145.6,53.7,101.3,58,66.5,92c-43.9,42.9-46.3,107.1-46.8,120.2
+          c-1.7,45.2,14.1,62.2,1.4,103.6C12.1,345.6,0.3,348.8,0,370.6z"/>
+	        <path d="M373.8,89.9c2.7-4.9,14.3-24.5,37.4-30.2c5.6-1.4,18.5-4.6,30.9,2.2c18.8,10.2,20,33.9,20.2,38.1c0.1,3.2,0.6,21.6-8.6,25.9
+	          c-6.9,3.3-13-4.5-24.5-1.4c-7.4,1.9-7.1,5.7-13.7,7.9c-12.5,4.2-32-3.2-39.6-18.7C371.2,103.7,372.8,94,373.8,89.9z"/>
+	        <path d="M432.1,133.8c-4.2,1.6-9.3,6-8.6,10.8c0.7,5.5,8.7,9.2,15.1,7.9c6.7-1.4,12.4-8.5,10.8-13.7
+	          C447.7,133.4,438.5,131.3,432.1,133.8z"/>
+            </clipPath>
+         </defs>
+      </svg>
+      <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 814 506">     
+       <use fill="#000" filter="url(#prefix__b)" xlink:href="#prefix__a"/>
+		<foreignObject clip-path="url(#mask)" width="100%" height="1000">
+		    <video class="video" autoplay loop muted>
+		        <source src="/assets/images/hero_video.mp4" type="video/mp4">
+		        Your browser does not support the video tag.
+		    </video>
+		</foreignObject>
+      </svg>
+    </div>
 
-<div id="contact">
-	<div class="content">
-		<span class="title">Hit Me Up!</span>
-		Email: <a href="mailto:amysuejen@gmail.com">amysuejen@gmail.com</a>
-		<div class="social_media">
-			<a href="https://twitter.com/itsamyjen" target="_blank" class="btn"><img src="/images/ic-twitter.png" alt="Twitter"></a>
-			<a href="https://www.linkedin.com/in/amyjen" target="_blank" class="btn"><img src="/images/ic-linkedin.png" alt="Linkedin"></a>
-			<a href="https://instagram.com/itsamyjen" target="_blank" class="btn"><img src="/images/ic-instagram.png" alt="Instagram"></a>
-		</div>
-	</div>
-</div>
+
 
 </body>
 </html>
