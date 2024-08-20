f7f5cb0175039b7f865504c951a61b1fb8835118
Amy <amysuejen@gmail.com>
2024-08-20 14:27:25 -0700

    adding animation effect on projects

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,18 @@
-$(function() {
+$(document).ready(function() {
+    $(window).on('scroll', function() {
+        $('.project').each(function() {
+            if ($(this).isInViewport()) {
+                $(this).addClass('show');
+            }
+        });
+    });
 
-
-
+    // Helper function to check if an element is in the viewport
+    $.fn.isInViewport = function() {
+        var elementTop = $(this).offset().top;
+        var elementBottom = elementTop + $(this).outerHeight();
+        var viewportTop = $(window).scrollTop();
+        var viewportBottom = viewportTop + $(window).height();
+        return elementBottom > viewportTop && elementTop < viewportBottom;
+    };
 });
